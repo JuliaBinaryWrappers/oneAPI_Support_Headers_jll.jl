561c68aaa6660b73c746c9793b2f4de3504c20b8
jlbuild <juliabuildbot@gmail.com>
2026-07-31 19:30:33 +0000

    oneAPI_Support_Headers_jll build 2025.3.1+0

--- src/oneAPI_Support_Headers_jll.jl
+++ src/oneAPI_Support_Headers_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("oneAPI_Support_Headers")
-JLLWrappers.@generate_main_file("oneAPI_Support_Headers", UUID("24f86df5-245d-5634-a4cc-32433d9800b3"))
+JLLWrappers.@generate_main_file("oneAPI_Support_Headers", Base.UUID("24f86df5-245d-5634-a4cc-32433d9800b3"))
 end  # module oneAPI_Support_Headers_jll
